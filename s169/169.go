ccc31e898e17f91612e03eb8f8e052dca2640a38
zfCode <zfcode@qq.com>
2020-04-16 16:28:39 +0800

    add 169

---
+++ s169/169.go
@@ -0,0 +1,59 @@
+package s169
+
+/*
+
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+思路：
+hashtable+打擂台 O(n)
+
+还有其他的利用数学思路的解法，但是感觉不通用，然后还有摩尔投票的解法
+摩尔投票的意思：遇到相同的就加1，遇到不同的就减1，减到0就重新换个数开始计数，总能找到最多的那个
+
+*/
+
+//hashtable
+//func majorityElement(nums []int) int {
+//	all := make(map[int]int)
+//	max :=0
+//	maxVal :=0
+//	for _,v:= range nums{
+//
+//
+//		all[v]+=1
+//		if v ==maxVal{
+//			max++
+//		}else {
+//			if all[v]>max{
+//				max = all[v]
+//				maxVal = v
+//			}
+//		}
+//
+//	}
+//	return maxVal
+//}
+
+//摩尔投票
+func majorityElement(nums []int) int {
+	max :=0
+	maxVal :=0
+	for _,v:= range nums{
+
+		if max == 0{
+			maxVal = v
+			max = 1
+		}else {
+			if maxVal == v {
+				max++
+			}else {
+				max--
+			}
+		}
+
+
+	}
+	return maxVal
+}
